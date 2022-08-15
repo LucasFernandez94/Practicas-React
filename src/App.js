4021aaed2f1b2c7ef8af812180ddad43b8716947
Lucas Fernandez <lucas.fernandez@daux.com.ar>
2022-08-15 19:30:23 -0300

    Commit Proyecto React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './pelicula';
+import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json';
+import Paginacion from './Paginacion';
+import { useState } from 'react';
 
 function App() {
+
+  const[pagiaActual, setPaginaActual] = useState(1);
+  const TOTAL_POR_PAGINA = 7;
+  let peliculas = peliculasJson;
+  const cargarPeliculas = ()=>{  
+    peliculas = peliculas.slice((pagiaActual) - 1 * TOTAL_POR_PAGINA, pagiaActual * TOTAL_POR_PAGINA);
+  }
+  const getTotalPaginas = ()=>{
+    let cabtidadTotalDePeliculas = peliculasJson.length;
+    return Math.ceil(cabtidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+cargarPeliculas();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      {peliculas.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
+          director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
+          img={pelicula.img}>
+          {pelicula.descripcion}
+        </Pelicula>
+      )}
+      <Paginacion pagina={pagiaActual} total={getTotalPaginas()} onChange={(pagina)=>{
+        setPaginaActual(pagina)
+      }}/>
+    </PageWrapper>
   );
 }
 
